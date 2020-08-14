144df45c71af75fb2f9d38a7ef63a20f353679f7
Minroe <scott.whitney334@gmail.com>
2020-08-13 17:36:46 -0700

    first commit we're switching up the node program to have a website gui for them to interact with

---
+++ assets/javascript/index.js
@@ -0,0 +1,13 @@
+
+
+
+$(".update").on("click", function() {
+  console.log("testing")
+})
+
+// var updateBackup = function() {
+//   return $.ajax({
+//     url: "/api/update",
+//     method: "POST"
+//   })
+// }
---
+++ server.js
@@ -0,0 +1,74 @@
+const express = require("express");
+const path = require("path");
+const csv = require('csv-parser');
+const fs = require('fs');
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+const csvWriter = createCsvWriter({
+  path: './RESULTS.csv',
+  header: [
+{id: 'TRACKINGNUMBER', title: 'TRACKINGNUMBER'},
+{id: 'ORDERNUMBER', title: 'ORDERNUMBER'},
+{id: 'DATE', title: 'DATE'},
+{id: 'RECEIVER', title: 'RECEIVER'},
+{id: 'COST', title: 'COST'},
+{id: 'WEIGHT', title: 'WEIGHT'},
+{id: 'RECEIVERORDERNUMBER', title: 'RECEIVERORDERNUMBER'}
+  ]
+}); 
+
+var results = [];
+var currentSearch = []
+var newSubFolder = []
+var updateResults = []
+
+const masterPath = 'UPS_CONFIRMATIONS.csv'
+const backupPath = 'UPS_DATA_BACKUP.csv'
+const backupFolderPath = 'UPS_DATA_ORGANIZED'
+const resultsPath = 'RESULTS.csv'
+
+
+var app = express();
+var PORT = 3001;
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "index.html"))
+});
+app.get("/search", function(req, res) {
+  res.sendFile(path.join(__dirname, "search.html"));
+});
+
+
+
+app.get("/api/update", function(req, res) {
+  console.log("begining backup generation using master CSV - this should be almost instant")
+  fs.copyFile(masterPath, backupPath, (err) => {
+    if (err) throw err;
+    console.log('backup file created')
+    console.log('double checking firing off initial setup checks')
+    
+  })
+  res.json("did it update?")
+});
+app.get("/api/search", function(req, res) {
+
+  console.log("looking for search results")
+  fs.createReadStream(resultsPath)
+  .pipe(csv())
+  .on('data', (data) => results.push(data))
+  .on('end', () => {
+    console.log('Data Ready hit arrow key');
+  });
+  res.json(results)
+  results = []
+})
+
+
+
+
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
